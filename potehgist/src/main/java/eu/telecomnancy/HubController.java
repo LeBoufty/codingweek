b3e90037e4811c414d2bf2ef1d33213cd7d31bd6
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2024-01-08 16:41:31 +0100

    Bismillah

--- potehgist/src/main/java/eu/telecomnancy/HubController.java
+++ potehgist/src/main/java/eu/telecomnancy/HubController.java
@@ -4,5 +4,8 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class HubController {
-
+    @FXML
+    private void depot() throws IOException {
+        App.setRoot("creationannonce");
+    }
 }
