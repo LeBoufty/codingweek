19f5241fe8c8d69f6974d57877608b4f365a38c1
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-08 15:07:14 +0100

    Add header_burger ChoiceBox and initialize it with options

--- potehgist/src/main/java/eu/telecomnancy/HeaderController.java
+++ potehgist/src/main/java/eu/telecomnancy/HeaderController.java
@@ -1,12 +1,34 @@
 package eu.telecomnancy;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class HeaderController {
+    
+    @FXML
+    private ChoiceBox<String> header_burger;
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+private void initialize() {
+    // Mapping between display names and corresponding FXML files
+    Map<String, String> pageMappings = new HashMap<>();
+    pageMappings.put("Connexion", "connect");
+    pageMappings.put("Creation Annonce", "creationannonce");
+    pageMappings.put("Creation Compte", "creationcompte");
+
+    header_burger.getItems().addAll("Connexion", "Creation Annonce", "Creation Compte");
+
+    header_burger.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+        try {   
+            App.setRoot(pageMappings.get(newValue));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    });
 }
+
+    }
