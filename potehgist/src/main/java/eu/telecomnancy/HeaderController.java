20eed9d461f3f53cd2b5e5dd4ee0c5f41479f245
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2024-01-08 09:49:45 +0100

    ajout header

---
+++ potehgist/src/main/java/eu/telecomnancy/HeaderController.java
@@ -0,0 +1,13 @@
+package eu.telecomnancy;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class HeaderController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
