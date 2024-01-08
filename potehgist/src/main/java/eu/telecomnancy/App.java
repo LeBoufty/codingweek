a674a1f2d464c063a74924b99add7c695d7a0c53
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2024-01-08 14:50:09 +0100

    zidjqf

--- potehgist/src/main/java/eu/telecomnancy/App.java
+++ potehgist/src/main/java/eu/telecomnancy/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("connect"), 640, 480);
+        scene = new Scene(loadFXML("profil"), 640, 480);
         stage.setTitle("PotehGist");
         stage.setScene(scene);
         stage.show();
