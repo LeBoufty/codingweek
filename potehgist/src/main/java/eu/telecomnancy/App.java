4826de4e05d926e64e9950aa16c56c10da43be10
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-08 11:29:13 +0100

    Add title to the application window

--- potehgist/src/main/java/eu/telecomnancy/App.java
+++ potehgist/src/main/java/eu/telecomnancy/App.java
@@ -18,6 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setTitle("PotehGist");
         stage.setScene(scene);
         stage.show();
     }
@@ -32,7 +33,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
