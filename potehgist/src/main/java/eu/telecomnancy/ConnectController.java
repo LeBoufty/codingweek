ad8af087c067fcf82c206c19c575ef379528c3f6
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2024-01-08 11:26:05 +0100

    Squelette connectcontroller

--- potehgist/src/main/java/eu/telecomnancy/ConnectController.java
+++ potehgist/src/main/java/eu/telecomnancy/ConnectController.java
@@ -12,9 +12,21 @@ public class ConnectController {
     @FXML
     private javafx.scene.control.TextField password;
 
+    // Vu que l'username peut peut-être contenir des espaces, on devrait peut-être utiliser l'email
     @FXML
     private void connect() throws IOException {
         System.out.println(username.getText()+" "+password.getText());
+        // On vérifie si le couple username/password est valide
+        // Si c'est invalide on affiche un message
+        // Sinon on redirige vers le hub avec userid de défini.
+    }
+
+    private boolean valide(String username, String password) {
+        return true;
+    }
+
+    private String getUserid(String username) {
+        return "1";
     }
     
 }
