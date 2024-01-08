240c5cd2439a94bdef77b2ac293583d0b88d62ad
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2024-01-08 11:09:40 +0100

    front connection

--- potehgist/src/main/java/eu/telecomnancy/App.java
+++ potehgist/src/main/java/eu/telecomnancy/App.java
@@ -1,12 +1,12 @@
 package eu.telecomnancy;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App test potagit
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("connect"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
---
+++ potehgist/src/main/java/eu/telecomnancy/ConnectController.java
@@ -0,0 +1,20 @@
+package eu.telecomnancy;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class ConnectController {
+
+    @FXML
+    private javafx.scene.control.TextField username;
+
+    @FXML
+    private javafx.scene.control.TextField password;
+
+    @FXML
+    private void connect() throws IOException {
+        System.out.println(username.getText()+" "+password.getText());
+    }
+    
+}
