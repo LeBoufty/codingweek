50aed0049b964a549220b7292d74a533ac0bfbf4
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-08 16:22:57 +0100

    Add "Deconnexion" option to header_burger menu

--- potehgist/src/main/java/eu/telecomnancy/HeaderController.java
+++ potehgist/src/main/java/eu/telecomnancy/HeaderController.java
@@ -22,12 +22,12 @@ private void initialize() {
     homeButton.setOnAction(this::handleHomeButtonClick);
     // Mapping between display names and corresponding FXML files
     Map<String, String> pageMappings = new HashMap<>();
-    pageMappings.put("Connexion", "connect");
     pageMappings.put("Creation Annonce", "creationannonce");
     pageMappings.put("Creation Compte", "creationcompte");
     pageMappings.put("Mon Profil", "profil");
+    pageMappings.put("Deconnexion", "connect");
 
-    header_burger.getItems().addAll("Connexion", "Creation Annonce", "Creation Compte", "Mon Profil");
+    header_burger.getItems().addAll("Connexion", "Creation Annonce", "Creation Compte", "Mon Profil", "Deconnexion");
 
     header_burger.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
         try {   
