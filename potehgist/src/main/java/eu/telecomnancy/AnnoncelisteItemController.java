c2b2f4b86a94af7092db3b6d8279426e6a5c1762
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-09 17:41:53 +0100

    Add AnnoncelisteItemController and update ListeAnnonceController

---
+++ potehgist/src/main/java/eu/telecomnancy/AnnoncelisteItemController.java
@@ -0,0 +1,42 @@
+package eu.telecomnancy;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+
+public class AnnoncelisteItemController{
+    
+    @FXML
+    private Button Button;
+
+    @FXML
+    private Label description;
+
+    @FXML
+    private ImageView img;
+
+    @FXML
+    private Label price;
+
+    @FXML
+    private Label title;
+
+    public void setData(Annonce annonce){
+        Image image = new Image(annonce.getImgSrc());
+        img.setImage(image);
+
+        title.setText(annonce.getTitre());
+        description.setText(annonce.getDescription());
+        price.setText(annonce.getPrix().toString());
+        
+    }
+
+    public void initialize(URL location, ResourceBundle ressources) {
+
+    }    
+}
