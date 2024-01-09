531d607cde0999d129a2266c7f22111baa58757b
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2024-01-09 19:24:32 +0100

    voila pour votre fix de merde

--- potehgist/src/main/java/eu/telecomnancy/HubController.java
+++ potehgist/src/main/java/eu/telecomnancy/HubController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import eu.telecomnancy.BDD_App.API;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HubController {
@@ -42,6 +43,21 @@ public class HubController {
     private Label page;
 
     @FXML
+    private Button bouton1;
+
+    @FXML
+    private Button bouton2;
+
+    @FXML
+    private Button bouton3;
+
+    @FXML
+    private Button boutonleft;
+
+    @FXML
+    private Button boutonright;
+
+    @FXML
     private void initialize()
     {
         
@@ -51,6 +67,14 @@ public class HubController {
         Date[] dates = API.getInstance().getthreedatesnotif(App.getUser().getId(), App.numpageannonce);
         ids = API.getInstance().getthreeidnotif(App.getUser().getId(), App.numpageannonce);
         if(messages!=null){
+            if(messages[0]==null && messages[1]==null && messages[2]==null)
+            {
+                bouton1.setVisible(false);
+                bouton2.setVisible(false);
+                bouton3.setVisible(false);
+                boutonleft.setVisible(false);
+                boutonright.setVisible(false);
+            }
             if(messages[0]!=null){
                 if(API.getInstance().checkvunotif(ids[0]))
             {
@@ -63,12 +87,14 @@ public class HubController {
                 description1.setText(messages[0]);
                 date1.setText(dates[0].toString());
                 API.getInstance().mettrenotifenvu(ids[0]);
+                 bouton1.setVisible(true);
             }
             else
             {
                 description1.setText("");
                 date1.setText("");
                 vu1.setText("");
+                bouton1.setVisible(false);
             }
             if(messages[1]!=null){
                 if(API.getInstance().checkvunotif(ids[1]))
@@ -82,12 +108,14 @@ public class HubController {
                 description2.setText(messages[1]);
                 date2.setText(dates[1].toString());
                 API.getInstance().mettrenotifenvu(ids[1]);
+                bouton2.setVisible(true);
             }
             else
             {
                 description2.setText("");
                 date2.setText("");
                 vu2.setText("");
+                bouton2.setVisible(false);
             }
             if(messages[2]!=null){
                 if(API.getInstance().checkvunotif(ids[2]))
@@ -101,12 +129,14 @@ public class HubController {
                 description3.setText(messages[2]);
                 date3.setText(dates[2].toString());
                 API.getInstance().mettrenotifenvu(ids[2]);
+                bouton3.setVisible(true);
             }
             else
             {
                 description3.setText("");
                 date3.setText("");
                 vu3.setText("");
+                bouton3.setVisible(false);
             }
         }
     }
@@ -142,12 +172,14 @@ public class HubController {
             description1.setText(messages[0]);
             date1.setText(dates[0].toString());
             API.getInstance().mettrenotifenvu(ids[0]);
+            bouton1.setVisible(true);
         }
         else
         {
             description1.setText("");
             date1.setText("");
             vu1.setText("");
+            bouton1.setVisible(false);
         }
         if(messages[1]!=null){
             if(API.getInstance().checkvunotif(ids[1]))
@@ -161,12 +193,14 @@ public class HubController {
             description2.setText(messages[1]);
             date2.setText(dates[1].toString());
             API.getInstance().mettrenotifenvu(ids[1]);
+            bouton2.setVisible(true);
         }
         else
         {
             description2.setText("");
             date2.setText("");
             vu2.setText("");
+            bouton2.setVisible(false);
         }
         if(messages[2]!=null){
             if(API.getInstance().checkvunotif(ids[2]))
@@ -180,6 +214,7 @@ public class HubController {
             description3.setText(messages[2]);
             date3.setText(dates[2].toString());
             API.getInstance().mettrenotifenvu(ids[2]);
+            bouton3.setVisible(true);
             
         }
         else
@@ -187,6 +222,7 @@ public class HubController {
             description3.setText("");
             date3.setText("");
             vu3.setText("");
+            bouton3.setVisible(false);
         }
     }
     }
@@ -220,6 +256,7 @@ public class HubController {
                 description1.setText("");
                 date1.setText("");
                 vu1.setText("");
+                bouton1.setVisible(false);
             }
             if(messages[1]!=null){
                 if(API.getInstance().checkvunotif(ids[1]))
