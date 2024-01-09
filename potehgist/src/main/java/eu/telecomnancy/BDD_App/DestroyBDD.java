a0785ea3e6c0970e7a28bc946b07a88facff933a
GUYOT Pierre <pierre.guyot@telecomnancy.eu>
2024-01-09 15:43:48 +0100

    Drop Database

---
+++ potehgist/src/main/java/eu/telecomnancy/BDD_App/DestroyBDD.java
@@ -0,0 +1,18 @@
+package eu.telecomnancy.BDD_App;
+
+public class DestroyBDD {
+
+    public static void main(String[] args) {
+
+        String url =  CreateBDD.BDD_NAME;
+
+        // Supprime le fichier de la BDD
+        java.io.File file = new java.io.File(url);
+        if (file.delete()) {
+            System.out.println("[DEBUG] La base de données a été supprimée.");
+        } else {
+            System.out.println("[DEBUG] La base de données n'a pas été supprimée.");
+        }
+    }
+    
+}
