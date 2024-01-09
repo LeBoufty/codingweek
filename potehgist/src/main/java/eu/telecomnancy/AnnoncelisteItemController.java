08d8a383401fdad35ce51f348e3f83fb6e6e9292
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-10 00:56:30 +0100

    Remove unused import statement

--- potehgist/src/main/java/eu/telecomnancy/AnnoncelisteItemController.java
+++ potehgist/src/main/java/eu/telecomnancy/AnnoncelisteItemController.java
@@ -6,7 +6,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.Image;
+//import javafx.scene.image.Image;
 
 public class AnnoncelisteItemController{
 
