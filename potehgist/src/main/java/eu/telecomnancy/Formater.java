3ce767f8a58c39c97efee898384123172e229ff0
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2024-01-09 09:23:40 +0100

    Formater

---
+++ potehgist/src/main/java/eu/telecomnancy/Formater.java
@@ -0,0 +1,39 @@
+package eu.telecomnancy;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class Formater {
+    public static final String USERNAME_REGEX = "^[a-zA-Z0-9_-]{4,32}$";
+    public static final int MIN_PASSWORD_LENGTH = 8;
+
+    public static boolean checkUsername(String username) {
+        return username.matches(USERNAME_REGEX);
+    }
+
+    public static boolean checkPassword(String password) {
+        return password.length() >= MIN_PASSWORD_LENGTH;
+    }
+
+    public static String format(String s) {
+        return s.replaceAll("'", "''");
+    }
+
+    public static String hash(String s) throws Exception {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedhash = digest.digest(s.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(encodedhash);
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
