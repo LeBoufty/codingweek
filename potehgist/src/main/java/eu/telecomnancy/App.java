d72e6699a35815afc18e372704b2fe33e421f33c
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2024-01-09 11:07:50 +0100

    Début css

--- potehgist/src/main/java/eu/telecomnancy/App.java
+++ potehgist/src/main/java/eu/telecomnancy/App.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +23,12 @@ public class App extends Application {
         scene = new Scene(loadFXML("connect"), 640, 480);
         stage.setTitle("PotehGist");
         stage.getIcons().add(new Image(getClass().getResourceAsStream("assets/logo.png")));
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        Font.loadFont(getClass().getResource("fonts/Roboto-Light.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("fonts/Roboto-Black.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("fonts/Roboto-Bold.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("fonts/Minecrafter.ttf").toExternalForm(), 10);
+        Font.loadFont(getClass().getResource("fonts/LEMONMILK-Bold.otf").toExternalForm(), 10);
         stage.setScene(scene);
         stage.show();
     }
