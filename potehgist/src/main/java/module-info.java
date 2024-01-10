55838e243576a27910eb383f1e0a1a54b678538f
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-10 17:37:38 +0100

    Add dependencies for imageio-core and javafx-swing

--- potehgist/src/main/java/module-info.java
+++ potehgist/src/main/java/module-info.java
@@ -3,8 +3,10 @@ module eu.telecomnancy {
     requires transitive javafx.graphics;
     requires transitive javafx.controls;
     requires transitive javafx.fxml;
+    requires transitive javafx.swing;
     requires transitive java.sql;
     requires transitive junit;
+    requires transitive java.desktop;
 
     opens eu.telecomnancy to javafx.fxml;
     exports eu.telecomnancy;
