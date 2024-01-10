1c8f0fb02478b7d9178bdbc232d3cd0eb6a76ee4
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-10 10:08:54 +0100

    Add getters and setters for Annonce class attributes

--- potehgist/src/main/java/eu/telecomnancy/Model/Annonce.java
+++ potehgist/src/main/java/eu/telecomnancy/Model/Annonce.java
@@ -88,4 +88,46 @@ public class Annonce {
     public void setPrix(Integer prix) {
         this.prix = prix;
     }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie.trim();
+    }
+
+    public String getDate_depot() {
+        return date_depot;
+    }
+
+    public void setDate_depot(String date_depot) {
+        this.date_depot = date_depot.trim();
+    }
+
+    public Utilisateur getVendeur() {
+        return vendeur;
+    }
+
+    public void setVendeur(Utilisateur vendeur) {
+        this.vendeur = vendeur;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+        maxid = Math.max(maxid, id);
+    }
+
+    public String getCode_postal() {
+        return code_postal;
+    }
+
+    public void setCode_postal(String code_postal) {
+        code_postal = code_postal.trim();
+        this.code_postal = code_postal;
+    }
 }
