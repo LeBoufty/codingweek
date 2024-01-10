1198fc8eb2c920f3e6de44a58f12b2097aee33a5
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-10 10:22:53 +0100

    Add labels for category, postal code, and deposit date in AnnoncelisteItemController

--- potehgist/src/main/java/eu/telecomnancy/AnnoncelisteItemController.java
+++ potehgist/src/main/java/eu/telecomnancy/AnnoncelisteItemController.java
@@ -12,6 +12,15 @@ import javafx.scene.image.ImageView;
 public class AnnoncelisteItemController{
 
     @FXML
+    private Label categorie;
+
+    @FXML
+    private Label code_postal;
+
+    @FXML
+    private Label date_depot;
+
+    @FXML
     private Label description;
 
     @FXML
@@ -30,6 +39,9 @@ public class AnnoncelisteItemController{
         title.setText(annonce.getTitre());
         description.setText(annonce.getDescription());
         price.setText(annonce.getPrix().toString());
+        categorie.setText(annonce.getCategorie());
+        date_depot.setText(annonce.getDate_depot());
+        code_postal.setText(annonce.getCode_postal());
 
     }
 
