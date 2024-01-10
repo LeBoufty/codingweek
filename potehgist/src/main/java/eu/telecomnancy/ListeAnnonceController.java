3ccde499f9833e959f3793dbb7fcffda3d960189
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-10 11:57:31 +0100

    Remove unused import statement

--- potehgist/src/main/java/eu/telecomnancy/ListeAnnonceController.java
+++ potehgist/src/main/java/eu/telecomnancy/ListeAnnonceController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import java.util.List;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import javafx.fxml.FXMLLoader;
 
