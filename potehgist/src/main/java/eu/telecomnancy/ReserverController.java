17a9617fa599182d142da9b1e63291b027c691fb
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-11 00:15:22 +0100

    Add labels and initialize method in ReserverController

--- potehgist/src/main/java/eu/telecomnancy/ReserverController.java
+++ potehgist/src/main/java/eu/telecomnancy/ReserverController.java
@@ -3,14 +3,32 @@ package eu.telecomnancy;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import eu.telecomnancy.Model.Annonce;
 
 public class ReserverController {
 
+    @FXML
+    private Label Description;
+
+    @FXML
+    private Label Prix;
+
+    @FXML
+    private Label Titre;
+    
     @FXML
     private DatePicker date_debut;
 
     @FXML
     private DatePicker date_fin;
+
+    public void initialize() throws Exception {
+        Annonce annonce = App.getAnnonce();
+        Description.setText(annonce.getDescription());
+        Prix.setText(annonce.getPrix().toString());
+        Titre.setText(annonce.getTitre());
+    }
 
     @FXML
     void reserver(ActionEvent event) {
