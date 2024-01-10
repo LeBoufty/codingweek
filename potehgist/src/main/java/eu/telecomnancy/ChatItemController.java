1f520c9abfe4999dcd0a5246638284cde5d98750
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2024-01-10 19:56:06 +0100

    efouqhfhq

---
+++ potehgist/src/main/java/eu/telecomnancy/ChatItemController.java
@@ -0,0 +1,30 @@
+package eu.telecomnancy;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ChatItemController {
+
+    @FXML
+    private Label Description;
+
+    @FXML
+    private Label Date;
+
+    public void setData(String description, int date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDateTime dateTime = LocalDateTime.ofEpochSecond(date, 0, java.time.ZoneOffset.UTC);
+        Description.setText(description);
+        Date.setText(dateTime.format(formatter));
+
+    }
+
+    public void initialize(URL location, ResourceBundle ressources) {
+
+    }    
+}
