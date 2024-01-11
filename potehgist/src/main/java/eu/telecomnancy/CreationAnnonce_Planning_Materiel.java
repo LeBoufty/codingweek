5ed63fd94b0902e84da928668e83a87cc447bed3
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2024-01-11 18:38:03 +0100

    pleins de fix

--- potehgist/src/main/java/eu/telecomnancy/CreationAnnonce_Planning_Materiel.java
+++ potehgist/src/main/java/eu/telecomnancy/CreationAnnonce_Planning_Materiel.java
@@ -1,10 +1,10 @@
 package eu.telecomnancy;
 
+import eu.telecomnancy.BDD_App.API;
+import eu.telecomnancy.Model.Date_M;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
-import eu.telecomnancy.BDD_App.API;
-import eu.telecomnancy.Model.Date_M;
 
 public class CreationAnnonce_Planning_Materiel {
 
@@ -16,14 +16,22 @@ public class CreationAnnonce_Planning_Materiel {
 
     @FXML
     void create_annonce(ActionEvent event) throws Exception {
-        System.out.println("Create annonce materiel");
-        App.annonce_en_creation.date_debut_materiel = Date_M.getDate_FXML(date_debut.getValue());
-        App.annonce_en_creation.date_fin_materiel = Date_M.getDate_FXML(date_fin.getValue());
-        API.getInstance().addOffre(App.annonce_en_creation);
-        System.out.println("Request sent to API to add annonce");
-        API.getInstance().addPlaningMateriel(App.annonce_en_creation);
-        System.out.println("Request sent to API to add planning");
-        App.setRoot("hub");
+        if(date_debut.getValue() == null || date_fin.getValue() == null || Date_M.getDate_FXML(date_debut.getValue())>Date_M.getDate_FXML(date_fin.getValue()))
+        {
+            System.out.println("Date non valide");
+        }
+        else
+        {
+            System.out.println("Create annonce materiel");
+            App.annonce_en_creation.date_debut_materiel = Date_M.getDate_FXML(date_debut.getValue());
+            App.annonce_en_creation.date_fin_materiel = Date_M.getDate_FXML(date_fin.getValue());
+            API.getInstance().addOffre(App.annonce_en_creation);
+            System.out.println("Request sent to API to add annonce");
+            API.getInstance().addPlaningMateriel(App.annonce_en_creation);
+            System.out.println("Request sent to API to add planning");
+            App.setRoot("hub");
+        }
+        
     }
 
 }
