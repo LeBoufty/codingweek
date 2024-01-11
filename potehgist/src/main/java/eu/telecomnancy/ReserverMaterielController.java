27c38a42b04d5dff926b71c7b355637f33df9f78
Titouan <titouan.langlais@telecomnancy.eu>
2024-01-11 16:30:13 +0100

    Fix reservation date issue and navigate to hub

--- potehgist/src/main/java/eu/telecomnancy/ReserverMaterielController.java
+++ potehgist/src/main/java/eu/telecomnancy/ReserverMaterielController.java
@@ -75,7 +75,8 @@ public class ReserverMaterielController {
     public void reserver() throws Exception{
         Date_M date_debut = new Date_M(this.date_debut.getValue().getYear(), this.date_debut.getValue().getMonthValue(), this.date_debut.getValue().getDayOfMonth(), Integer.parseInt(heure_debut.getText()), Integer.parseInt(minute_debut.getText()));
         Date_M date_fin = new Date_M(this.date_fin.getValue().getYear(), this.date_fin.getValue().getMonthValue(), this.date_fin.getValue().getDayOfMonth(), Integer.parseInt(heure_fin.getText()), Integer.parseInt(minute_fin.getText()));
-        API.getInstance().addPreReservation(App.getUser().getId(), App.getAnnonce().getId(),(int)date_debut.getDate(),(int)date_fin.getDate());; //uid ido dd df addpre
+        API.getInstance().addPreReservation(App.getUser().getId(), App.getAnnonce().getId(),(int)date_debut.getDate(),(int)date_fin.getDate());
+        App.setRoot("hub");
     }
 }
 
